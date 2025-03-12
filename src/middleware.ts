130d3a5234b18b2e6e971f8098fad4d251c7af4f
Dan Choiniere <dchoinie@gmail.com>
2025-03-12 15:18:09 -0500

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,5 +6,5 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
